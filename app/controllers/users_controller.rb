11e4c16775eab8cda250db61f583c6e759aa1c24
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-13 13:16:47 -0400

    minor changes to users signin flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,37 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		
+		@user = User.find_by(id: params[:id])
 	end
     
     def edit
-    	
+    	@user = User.find_by(id: params[:id])
     end
 
     #POST
 	def create
-		
+		@user = User.create(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to root_path
+		else
+			render :new
+		end
 	end
 
 	def update
-		
+		@user = User.find_by(id: params[:id])
+		@user.update(user_params)
+		if @user.save
+			redirect_to root_path
+		else
+			render :edit
+		end
 	end
+    
+	private 
 
-	def destroy
-		
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
