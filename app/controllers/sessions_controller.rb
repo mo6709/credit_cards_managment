93ddacf409ca0f24daad5db7b776a4f655fe7a50
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-13 17:48:09 -0400

    fix sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,20 @@
 class SessionsController < ApplicationController
 	def new
+		@user = User.new
 	end
 
 	def create
-		raise params.inspect
+		@user = User.find_by(:email => params[:user][:email])
+		if @user.try(:authenticate, params[:user][:password])
+			session[:user_id] = @user.id
+			redirect_to root_path
+		else
+			render :new
+		end
 	end
 
 	def destroy
-		
+		session.delete :user_id
+		redirect_to root_path
 	end
 end
