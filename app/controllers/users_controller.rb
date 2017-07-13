6012934a693cd63a61006fcc30c7c9e8ab4bfe5b
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-13 11:50:38 -0400

    creating new user in view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+	#GET 
+	def new
+		@user = User.new
+	end
+
+	def show
+		
+	end
+    
+    def edit
+    	
+    end
+
+    #POST
+	def create
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
 end
