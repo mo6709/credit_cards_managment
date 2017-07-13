318ec23a941e78effa311fbe9148f09ee92338e1
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-13 11:45:52 -0400

    schema changed renamed coulmn password_digest in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712214816) do
+ActiveRecord::Schema.define(version: 20170713152934) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "account_number"
@@ -62,9 +62,9 @@ ActiveRecord::Schema.define(version: 20170712214816) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "passwor_digest"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
