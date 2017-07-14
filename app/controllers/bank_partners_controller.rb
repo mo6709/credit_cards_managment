f25e5117ca55168e2f68194d750efdd427909db6
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-14 16:23:05 -0400

    ceaned bank_partners conntroller

--- app/controllers/bank_partners_controller.rb
+++ app/controllers/bank_partners_controller.rb
@@ -20,14 +20,4 @@ class BankPartnersController < ApplicationController
 
     def update
     end
-end
-
-
- # bank_partners#index
- #                       POST   /bank_partners(.:format)               bank_partners#create
- #      new_bank_partner GET    /bank_partners/new(.:format)           bank_partners#new
- #     edit_bank_partner GET    /bank_partners/:id/edit(.:format)      bank_partners#edit
- #          bank_partner GET    /bank_partners/:id(.:format)           bank_partners#show
- #                       PATCH  /bank_partners/:id(.:format)           bank_partners#update
- #                       PUT    /bank_partners/:id(.:format)           bank_partners#update
- #                       DELETE /bank_partners/:id(.:format)           bank_partners#destroy
+end	
