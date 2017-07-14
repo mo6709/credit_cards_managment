6526bea8466f963dc062086bece32e277955ad1a
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-14 11:15:25 -0400

    fix type class method in card class

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,11 +3,11 @@ class Card < ActiveRecord::Base
 	has_many :bank_partners, :through => :card_bank_partners
 	belongs_to :category
 
-	enum :type => [:personal, :business]
+	enum :c_type => [:personal, :business]
 
 	def self.type(c_type)
-		Card.all.map do |card|
-			card.type == c_type
+		Card.all.find_all do |card|
+			card.c_type == c_type
 		end
 	end
 end
