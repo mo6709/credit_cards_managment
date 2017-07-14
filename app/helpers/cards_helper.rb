f1579a0bd58574d659d9c1d6df9dbf460986bdf1
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-14 15:04:38 -0400

    added links_for_bank_partners(card) helper method to card

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,8 @@
 module CardsHelper
-end
+	def links_for_bank_partners(card)
+      links = card.bank_partners.map do |bank| 
+      	        link_to(bank.name, bank_partner_path(bank.id)) 
+              end
+      links.join(" | ").html_safe        
+	end
+end	
