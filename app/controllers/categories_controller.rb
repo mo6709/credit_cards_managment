6e1c4e5bb3b317481b91c3a0f2b842210d269165
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-14 16:51:06 -0400

    added controller and view featurs for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,23 @@
 class CategoriesController < ApplicationController
+    #GET
+	def index
+		@categories = Category.all
+	end
+
+	def new
+	end
+
+	def show
+		@category = Category.find_by(:id => params[:id])
+	end
+    
+    def edit
+    end
+
+    #POST
+	def creat
+	end
+
+	def update
+	end
 end
