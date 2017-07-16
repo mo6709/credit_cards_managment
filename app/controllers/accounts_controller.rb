458a63993394a44169477a909dc9c09246861dbf
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-16 11:38:50 -0400

    added nested index to accounts controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,19 @@
 class AccountsController < ApplicationController
 	#GET
+	def index
+		if params[:user_id]
+			@user = User.find_by(:id => params[:user_id])
+			if @user.nil?
+				redirect_to login_path
+			else
+				@accounts = @user.accounts
+			end
+		else
+		end
+	end
+
 	def new
-		
+		@account = Account.new
 	end
 
 	def show
@@ -25,3 +37,12 @@ class AccountsController < ApplicationController
 		
 	end
 end
+
+         user_accounts GET    /users/:user_id/accounts(.:format)          accounts#index
+                       POST   /users/:user_id/accounts(.:format)          accounts#create
+      new_user_account GET    /users/:user_id/accounts/new(.:format)      accounts#new
+     edit_user_account GET    /users/:user_id/accounts/:id/edit(.:format) accounts#edit
+          user_account GET    /users/:user_id/accounts/:id(.:format)      accounts#show
+                       PATCH  /users/:user_id/accounts/:id(.:format)      accounts#update
+                       PUT    /users/:user_id/accounts/:id(.:format)      accounts#update
+                       DELETE /users/:user_id/accounts/:id(.:format)      accounts#destroy
