761b20f0685ed710cd5f5b4ec109586db1796480
MOSHE LEVI <Moishy@MOSHEs-MacBook-Air.local>
2017-07-17 10:44:33 -0400

    added admin featue to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714164720) do
+ActiveRecord::Schema.define(version: 20170717144120) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "account_number"
@@ -64,8 +64,9 @@ ActiveRecord::Schema.define(version: 20170714164720) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
