8c564b4ce76934934c1a17f4811426303041d470
Moshe Levi <mo6709@gmail.com>
2017-07-18 12:16:20 -0400

    aded nested form for bank_partner with card info

--- app/models/bank_partner.rb
+++ app/models/bank_partner.rb
@@ -1,4 +1,17 @@
 class BankPartner < ActiveRecord::Base
 	has_many :card_bank_partners
-	has_many :cards, :through => :card_bank_partners
+	has_many :cards, through: :card_bank_partners
+
+	def cards_attributes=(card_attributes)
+	  	card_attributes.each do |i, attributes|
+	  		if attributes[:name].present?
+		  		card = Card.find_or_create_by(:name => attributes[:name])
+		  		if !self.cards.include?(card)
+		  		    self.card_bank_partners.build(:card => card)
+		  		    self.save
+		  		end
+	  		end
+	  	end
+    end
+
 end
