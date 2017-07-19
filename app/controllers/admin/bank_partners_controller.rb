d104876939628bd4c0a6de0954ffb70bcae0672e
Moshe Levi <mo6709@gmail.com>
2017-07-19 12:47:46 -0400

    fixed bank_partner controller in admin scope

--- app/controllers/admin/bank_partners_controller.rb
+++ app/controllers/admin/bank_partners_controller.rb
@@ -7,6 +7,7 @@ class Admin::BankPartnersController < ApplicationController
 
 	def new
 		@bank_partner = BankPartner.new
+		@card = Card.new
 	end
 
 	def edit
@@ -14,14 +15,18 @@ class Admin::BankPartnersController < ApplicationController
 	end
     
     def show
-    	
+    	@bank_partner = BankPartner.find_by(:id => params[:id])
     end
 
     #POST
 
     def create
     	@bank_partner = BankPartner.create(bank_partner_params)
-    	raise @bank_partner.inspect
+    	if @bank_partner.valid?
+    	    redirect_to admin_bank_partner_path(@bank_partner.id), alert: 'Bank Partner was successfully created'
+    	else
+    		render 'new'
+    	end
     end
 
     def update
@@ -34,7 +39,10 @@ class Admin::BankPartnersController < ApplicationController
 
     private 
     def bank_partner_params
-    	params.require(:bank_partner).permit(:name, :card_ids => [], :cards_attributes => [:name])
+    	params.require(:bank_partner).permit(:name, :card_ids => [], 
+    		                                 :cards_attributes => 
+    		                                 [:name, :c_type, :bonus, :apr, :anual_fee, :credit_needed, :balance_transfer_apr, :balance_transfer_period, :category_id, :corp_url]
+    		                                 )
     end
 
 end
