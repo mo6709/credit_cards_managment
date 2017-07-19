a80af3454a49770cf5a205f76817a486f4906a4e
Moshe Levi <mo6709@gmail.com>
2017-07-19 17:23:16 -0400

    fixed admin_access method in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,11 @@ module ApplicationHelper
   	User.find_by(:id => session[:user_id])
   end
 
-  def admin_access
-    current_user.admin
+  def admin_access  
+      unless current_user.try(:admin)
+  		flash[:alert] = "You most be logged in as an admin to access this section"
+  		redirect_to root_path
+  	  end
   end
+
 end
