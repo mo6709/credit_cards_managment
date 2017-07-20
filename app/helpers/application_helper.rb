a10d4d1b3915b41f666d2bcc90535987aa21c5a0
Moshe Levi <mo6709@gmail.com>
2017-07-20 13:14:32 -0400

    added loggedin_buttons_for() in the application helper and invoke them in account/accounts partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,16 @@ module ApplicationHelper
   end
 
   def admin_access  
-      unless current_user.try(:admin)
-  		flash[:alert] = "You most be logged in as an admin to access this section"
-  		redirect_to root_path
-  	  end
+	unless current_user.try(:admin)
+		flash[:alert] = "You most be logged in as an admin to access this section"
+		redirect_to root_path
+	end
+  end
+
+  def loggedin_buttons_for(account)
+  	if logged_in?
+      return [button_to("Edit", edit_user_account_path(current_user.id, account.id), method: :get),button_to("Delete", "/users/#{current_user.id}/accounts/#{account.id}", method: :delete)].join.html_safe
+  	end
   end
 
 end
