fdae47db45c394c8eeca960d067b99624be6a57c
Moshe Levi <mo6709@gmail.com>
2017-07-21 17:24:12 -0400

    added validations to card

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,12 @@ class Card < ActiveRecord::Base
 	has_many :card_bank_partners
 	has_many :bank_partners, through: :card_bank_partners
 	belongs_to :category
+    
+    validates :name, :uniqueness => true
+    NON_VALIDATABLE_ATTR = ["id", "created_at", "updated_at"]
+	VALIDATABLE_ATTR = self.attribute_names.reject{|attr| NON_VALIDATABLE_ATTR.include?(attr)}
+	validates_presence_of VALIDATABLE_ATTR
+
 
 	enum :c_type => [:personal, :business]
 
