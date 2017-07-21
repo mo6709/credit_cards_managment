8764924f70f270b082ed94327d6ab91e07e630aa
Moshe Levi <mo6709@gmail.com>
2017-07-21 16:52:51 -0400

    added validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 	has_many :accounts
 	has_many :cards, :through => :accounts
 	has_secure_password
+
+    validates :email, :presence => true
+    validates :name, :presence => true
+    validates :password, :length => { :minimum => 7 }
+    validates :password, :confirmation => true
 end
