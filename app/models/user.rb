a318ffe6b3aa6f85b068f91df29f78427e579dd3
Moshe Levi <mo6709@gmail.com>
2017-07-21 17:32:35 -0400

    added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
     validates :email, :presence => true
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
     validates :name, :presence => true
     validates :password, :length => { :minimum => 7 }
     validates :password, :confirmation => true
