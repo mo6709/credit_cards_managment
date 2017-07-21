f3b023c3aa2b802209f9848673641c04954e0bdd
Moshe Levi <mo6709@gmail.com>
2017-07-21 16:50:49 -0400

    added validations to account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,9 @@
 class Account < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :card
+	validates :account_number, :uniqueness => true
+
+	NON_VALIDATABLE_ATTR = ["id", "created_at", "updated_at"]
+	VALIDATABLE_ATTR = self.attribute_names.reject{|attr| NON_VALIDATABLE_ATTR.include?(attr)}
+	validates_presence_of VALIDATABLE_ATTR
 end
