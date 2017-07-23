7b8132d26f4e87a6f1d0e7574c72581eaa881cf2
Moshe Levi <mo6709@gmail.com>
2017-07-23 13:12:37 -0400

    added find_or_create_by_omniauth() method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ActiveRecord::Base
     validates :name, :presence => true
     validates :password, :length => { :minimum => 7 }
     validates :password, :confirmation => true
+
+    def self.find_or_create_by_omniauth(auth_hash)
+    	self.where(:uid => auth_hash[:uid]).first_or_create do |user|
+    		user.uid = auth_hash[:uid]
+    		user.name = auth_hash[:info][:name]
+    		user.email = auth_hash[:info][:email]
+    		user.image = auth_hash[:info][:image]
+    	end
+    end
 end
