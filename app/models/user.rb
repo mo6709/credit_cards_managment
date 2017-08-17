c1bc3fdffb79aafe83dc492803bb0af1c1a4d6d6
Moshe Levi <mo6709@gmail.com>
2017-08-17 10:55:11 -0400

    fixed the find_or_create_by_omniauth methed for user with string for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    
 	has_many :accounts
 	has_many :cards, :through => :accounts
 	has_secure_password
@@ -6,7 +7,6 @@ class User < ActiveRecord::Base
     validates :email, :presence => true, :uniqueness => true
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
     validates :name, :presence => true
-
     validates :password, :length => { :minimum => 7 }
     validates :password, :confirmation => true
 
@@ -16,10 +16,10 @@ class User < ActiveRecord::Base
     		user.name = auth_hash[:info][:name]
     		user.email = auth_hash[:info][:email]
     		user.image = auth_hash[:info][:image]
-
+            
     		random_password = Random.new_seed
-    		user.password = random_password
-    		user.password_confirmation = random_password
+    		user.password = random_password.to_s
+    		user.password_confirmation = random_password.to_s
     	end
     end
 end
