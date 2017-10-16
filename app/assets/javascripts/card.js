e445c0227ca693c717b5f12727889e1dea4330a5
Moshe Levi <mo6709@gmail.com>
2017-10-16 18:23:40 -0400

    added card.js. created a Card object with its properties

---
+++ app/assets/javascripts/card.js
@@ -0,0 +1,13 @@
+function Card(attributes){
+    this.title = attributes.title;
+    this.name = attributes.name;
+    this.c_type = attributes.c_type;
+    this.bonus = attributes.bonus;
+    this.apr = attributes.apr;
+    this.anual_fee = attributes.anual_fee;
+    this.credit_needed = attributes.credit_needed;
+    this.balance_transfer_apr = attributes.balance_transfer_apr;
+    this.category = attributes.category;
+    this.bank_partners = attributes.bank_partners;
+};
+
