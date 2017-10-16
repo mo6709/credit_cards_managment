09fd5a3e7a048e74c122b0c336586edf7502240d
Moshe Levi <mo6709@gmail.com>
2017-10-16 14:03:19 -0400

    added costumized nested attributes in BankPartner #cards_attributes

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ActiveRecord::Base
 	has_many :bank_partners, through: :card_bank_partners
 	belongs_to :category
     
-    accepts_nested_attributes_for :bank_partners
+    # accepts_nested_attributes_for :bank_partners
     
     enum :c_type => [:personal, :business]
 
@@ -11,6 +11,18 @@ class Card < ActiveRecord::Base
     NON_VALIDATABLE_ATTR = ["id", "created_at", "updated_at"]
 	VALIDATABLE_ATTR = self.attribute_names.reject{|attr| NON_VALIDATABLE_ATTR.include?(attr)}
 	validates_presence_of VALIDATABLE_ATTR
+    
+
+    def bank_partners_attributes=(bank_partners_attributes)
+		bank_partners_attributes.each do |i, attributes|
+            if attributes[:name].present?
+                bank_partner = BankPartner.find_or_create_by(:name => attributes[:name])
+                if !self.bank_partners.include?(bank_partner)
+                    self.bank_partners << bank_partner
+                end
+            end
+		end
+    end
 
 	def self.type(c_type)
 		Card.all.find_all do |card|
