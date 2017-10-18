d10d5d68fb4fe886d5da444a76aec959d2e6180f
Moshe Levi <mo6709@gmail.com>
2017-10-18 13:11:14 -0400

    fixed card_serializer class

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,5 +1,5 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :name, :c_type, :bonus, :apr, :anual_fee, :credit_needed, :balance_transfer_apr 
+  attributes :id, :name, :c_type, :bonus, :apr, :anual_fee, :credit_needed, :balance_transfer_apr, :corp_url, :balance_transfer_period
   belongs_to :category
   has_many :bank_partners, serializer: CardBankPartnersSerializer
 end
