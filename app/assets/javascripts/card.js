e245061665eac766b3393b15e0a5f50c2abcc5e8
Moshe Levi <mo6709@gmail.com>
2017-10-18 16:45:19 -0400

    added render handlebars functions for modal in card.js

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,6 +1,7 @@
 var cards = {};
 
 function Card(attributes){
+	this.id = attributes.id;
     this.name = attributes.name;
     this.c_type = attributes.c_type;
     this.bonus = attributes.bonus;
@@ -22,26 +23,39 @@ Card.getCard = function(cardNode){
     })
     .success(function(data){
     	var newCard = Card.makeCard(data)
-    	Card.showCard(newCard)
+    	$('div.cards_list').append(newCard.renderModal());
+    	Card.showCard(newCard.id)
     }) 
 };
 
-Card.showCard = function(cardObject){ 
-	// make a modal appear on the page with handelbars
+Card.showCard = function(cardId){ 
+   //change the css default to show the card with the corect id
+   $(`#modalCard${cardId}`)[0].style.display = "block";
 };
 
 Card.makeCard = function(json){
     var card = new Card(json);
     cards[card.id] = card;
+    return card;
 };
 
 
+Card.prototype.renderModal = function(){
+    return Card.modalTemplate(this)
+};
+
+
+
 $(function(){
+
+    Card.modalTemplateSource = $('#modalTemplate').html();
+    Card.modalTemplate = Handlebars.compile(Card.modalTemplateSource); 
+
 	$('a.card_name').on("click", function(event){
 		event.preventDefault();
         
 	    if(cards[this.dataset.id]){
-		    Card.showCard(cards[this.dataset.id])
+		    Card.showCard(this.dataset.id)
 		}else{
 			Card.getCard(this)
 		}		
